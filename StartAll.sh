0725ef5c4b978b61ceddd9f87821c2e38651ef32
Val <valerii.tikhonov@klarna.com>
2016-12-05 20:58:42 +0100

    add game service to startall script

--- StartAll.sh
+++ StartAll.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# database
-docker run --name mongodb -d mongo:3.3
-
-# cache
-docker run --name redis -d redis:3
-
-# configuration
-docker build --tag seabattle/etcd Etcd/
-docker run -p 2379:2379 --name etcd -d seabattle/etcd
-
 # consul
-docker run consul 
+docker run -d --net=host -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true}' consul agent -server -bind=127.0.0.1 -bootstrap
 
-# user service
-docker build --tag seabattle/user_service UserService/
-docker run --name user_service --link mongodb:mongo --link redis:redis --link etcd:etcd -h user_service.sb -t -i -p 8080:8080 seabattle/user_service /bin/bash
+# add game for config
+curl -X PUT -d 'seabattle' "http://127.0.0.1:8500/v1/kv/game_service/games"
+
+# game service
+docker build . -t seabattle/game_service
+docker run -d -p 8080:8080 --name game_service seabattle/game_service
+
+# registrator
+docker run -d \
+    --name=registrator \
+    --net=host \
+    --volume=/var/run/docker.sock:/tmp/docker.sock \
+    gliderlabs/registrator:latest \
+      consul://localhost:8500
--- UserService/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/opt/app/SeaUsers-master/_rel/seausers_release/bin console 
